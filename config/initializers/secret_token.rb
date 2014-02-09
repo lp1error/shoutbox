07fee37a02105cd8b5511146ff9c37cc8c458884
lp1error <lp1error@countermail.com>
2014-02-09 16:09:51 +0100

    second upload
    What up?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studentshoutout::Application.config.secret_key_base = 'fc9fa7d0cf702edbd630480c83c7b303daa33642961b386f93704285967f92d9fe04b3caee98e141105b5834de58f7ba396077dc4118cdc4502d45a18ec5c71c'
