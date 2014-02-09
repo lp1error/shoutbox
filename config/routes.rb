3064995a31f7da80890be37bc4ea5a531d165b20
lp1error <lp1error@countermail.com>
2014-02-09 20:27:17 +0100

    Fifth

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,24 @@ ActionController::Routing::Routes.draw do |map|
     
     # Sample of regular route:
     # map.connect 'products/:id', :controller => 'catalog',
+    :action => 'products'
     # Keep in mind you can assign values other than
     # :controller and :action
     
     # Sample of named route:
     # map.purchase 'products/:id/purchase', :controller => 'catalog',
-    :action => 'purchase'
     :action => 'products'
     # This route can be invoked with purchase_url(:id => product.id)
     
     # You can have the root of your site routed by hooking up ''
     # -- just remember to delete public/index.html.
     # map.connect '', :controller => "welcome"
-    
+    Blog::Application.routes.draw do
+ 
+  resources :posts
+ 
+  root to: "welcome#index"
+end
     # Allow downloading Web Service WSDL as a file with an extension
     
     # instead of a file named 'wsdl'
